770f25c70d023f28dd88e81b034218225be16c1b
KleprlikJan <kleprlikjan@gmail.com>
2020-08-10 23:07:36 +0200

    Add IAudioFormat

---
+++ Shazam/Audio/IAudioFormat.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shazam.Audio
+{
+	public interface IAudioFormat
+	{
+		public short Channels { get; set; }
+		public int SampleRate { get; set; }
+		public int ByteRate { get; set; }
+		public short BlockAlign { get; set; }
+		public short BitsPerSample { get; set; }
+		public int NumOfDataBytes { get; set; }
+		public byte[] Data { get; set; }
+
+		public bool IsCorrectFormat(byte[] data);
+	}
+}

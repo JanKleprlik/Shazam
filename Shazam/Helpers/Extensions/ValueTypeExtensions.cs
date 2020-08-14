ffa0ef92fc8fdceac4c50b8830cea280cd07345a
KleprlikJan <kleprlikjan@gmail.com>
2020-08-14 15:41:25 +0200

    Add Helper methods

---
+++ Shazam/Helpers/Extensions/ValueTypeExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Shazam.Extensions
+{
+	public static class ValueTypeExtensions
+	{
+		public static T Swap<T>(this T first, ref T second)
+		{
+			T tmp = second;
+			second = first;
+			return tmp;
+		}
+
+		public static int ToPowOf(this int _base, int exp) 
+		{
+			int res = _base;
+			for (int i = 0; i < exp; i++)
+			{
+				res *= _base;
+			}
+
+			return res;
+		}
+
+		public static double Pow2(this double _base)
+		{
+			return _base * _base;
+		}
+
+	}
+}
---
+++ Shazam/Helpers/Tools/Tools.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Shazam.Extensions;
+
+namespace Shazam.Tools
+{
+	public static class Tools
+	{
+		public static double GetComplexAbs(double real, double img)
+		{
+			return Math.Sqrt(real.Pow2() + img.Pow2());
+		}
+
+		public static bool IsPowOfTwo(int n)
+		{
+			if ((n & (n - 1)) != 0)
+				return false;
+			return true;
+		}
+	}
+}

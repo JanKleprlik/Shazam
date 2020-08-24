7405463a98479ffe1d5359f123a415c0360a9411
KleprlikJan <kleprlikjan@gmail.com>
2020-08-24 10:47:01 +0200

    Implement songfiltering and time coherency check

--- Shazam/Shazam.cs
+++ Shazam/Shazam.cs
@@ -6,6 +6,7 @@ using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Linq;
 using System.Net.Sockets;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading;
@@ -27,7 +28,6 @@ namespace Shazam
 		private const int anchorOffset = 2;
 
 
-		private Dictionary<Address, List<TargetZone>> database = new Dictionary<Address, List<TargetZone>>();
 		/// <summary>
 		/// Bits are from left to right
 		/// <para>Key:</para>
@@ -38,7 +38,7 @@ namespace Shazam
 		/// <para>32 bits absolute time of anchor</para>
 		/// <para>32 bits id of a song</para>
 		/// </summary>
-		private Dictionary<uint, List<ulong>> databaseSmall = new Dictionary<uint, List<ulong>>();
+		private Dictionary<uint, List<ulong>> database = new Dictionary<uint, List<ulong>>();
 
 
 		/// <summary>
@@ -91,12 +91,56 @@ namespace Shazam
 			}
 			/**/
 			AddTFpointsToDatabase(TimeFrequencyPoitns, songID);
-
+			SaveTFpoints(TimeFrequencyPoitns, songID);
 
 
 			#endregion
 		}
 
+		public void LoadSong(uint songID)
+		{
+			List<Tuple<uint, uint>> timeFrequencyPoints = LoadFrequencyPoints($"Fingerprints/{songID}.txt");
+
+			AddTFpointsToDatabase(timeFrequencyPoints, songID);
+		}
+
+		private List<Tuple<uint, uint>> LoadFrequencyPoints(string path)
+		{
+			//TODO: test if it exists else argument error ?
+
+			List<Tuple<uint, uint>> TFpoints = new List<Tuple<uint, uint>>();
+			using (StreamReader sr = new StreamReader(path))
+			{
+				while (true)
+				{
+					string line = sr.ReadLine();
+					if (line == null)
+						break;
+					var tokens = line.Split(';');
+
+					uint.TryParse(tokens[0],out uint time);
+					uint.TryParse(tokens[1],out uint frequency);
+
+					TFpoints.Add(new Tuple<uint, uint>(time, frequency));
+				}
+			}
+
+			return TFpoints;
+		}
+
+		private void SaveTFpoints(List<Tuple<uint, uint>> timeFrequencyPoitns, in uint songId)
+		{
+			//TODO: check if such file already exists
+
+
+			using (StreamWriter sw = new StreamWriter($"Fingerprints/{songId}.txt"))
+			{
+				foreach (var TFpoint in timeFrequencyPoitns)
+				{
+					sw.WriteLine(TFpoint.Item1 +";"+ TFpoint.Item2);
+				}
+			}
+		}
 
 
 		public string RecognizeSong()
@@ -133,38 +177,214 @@ namespace Shazam
 			#endregion
 
 			List<Tuple<uint, uint>> timeFrequencyPoints;
-
 			#region Creating Time-frequency points
 			int bufferSize = windowSize / downSampleCoef;
 			timeFrequencyPoints= CreateTimeFrequencyPoints(bufferSize, data, coefficient:1.1); //set higher coefficient because microphone has lower sensitivity
 			#endregion
 
-			/*/
-			for (int i = 0; i < timeFrequencyPoints.Count; i++)
-			{
-				Console.WriteLine($"index: {i}  --- time: {timeFrequencyPoints[i].Item1} --- frequency: {timeFrequencyPoints[i].Item2}");
-			}
-			/**/
+			Dictionary<uint, Dictionary<uint, List<uint>>> filteredSongs;
+			#region Filtering songs
 
 			//[address;(AbsAnchorTimes)]
 			Dictionary<uint, List<uint>> recordAddresses = CreateRecordAddresses(timeFrequencyPoints);
 
-			var PotentialDoubles = GetPotentialSongs(recordAddresses);
+			//get quantities of each SongValue to determine wether they make a complete TGZ
+			var quantities = GetSongValQuantities(recordAddresses);
 
+			//filter songs and addresses that only 
+			filteredSongs = FilterSongs(recordAddresses, quantities);
 
-			//check time coherency
+			#endregion
 
+			uint finalSong;
+			#region Choose best song by time coherency
+			//get longest delta for each song that says how many notes from recording are time coherent to the song
+			//[sognID, delta]
+			var deltas = GetDeltas(filteredSongs, recordAddresses);
 
+			//pick the songID with highest delta
+			finalSong = FilterLongestDelta(deltas);
+			#endregion
 
-
-			return "Idk really";
+			return finalSong.ToString();
 		}
+
+		private uint FilterLongestDelta(Dictionary<uint, uint> deltas)
+		{
+			uint songID = 0; //dummy initialization
+			uint longestDelta = 0;
+			bool initialized = false;
+			foreach (var pair in deltas)
+			{
+				if (pair.Value > longestDelta)
+				{
+					longestDelta = pair.Value;
+					songID = pair.Key;
+					initialized = true;
+				}
+			}
+
+			if (initialized)
+				return songID;
+
+			throw new ArgumentException("None song had any time coherent notes.");
+			
+		}
+
+		private Dictionary<uint, uint> GetDeltas(Dictionary<uint, Dictionary<uint, List<uint>>> filteredSongs, Dictionary<uint, List<uint>> recordAddresses)
+		{
+			//[songID, delta]
+			Dictionary<uint, uint> maxDeltas = new Dictionary<uint, uint>();
+			foreach (var songID in filteredSongs.Keys)
+			{
+				//[delta, occurence]
+				Dictionary<uint, int > songDeltasQty = new Dictionary<uint, int>();
+				foreach (var address in recordAddresses.Keys)
+				{
+					if (filteredSongs[songID].ContainsKey(address))
+					{
+						foreach (var absSongAnchTime in filteredSongs[songID][address]) //foreach AbsSongAnchorTime at specific address
+						{
+							foreach (var absRecAnchTime in recordAddresses[address]) //foreach AbsRecordAnchorTime at specific address
+							{
+								//satisfies: "songDelta >= recordDelta"	because song should be longer 10 secs
+								uint delta = absSongAnchTime - absRecAnchTime;
+								if (!songDeltasQty.ContainsKey(delta))
+									songDeltasQty.Add(delta, 0);
+								songDeltasQty[delta]++;
+							}
+						}
+					}
+				}
+
+				//get max delta
+				uint maxDelta = MaximizeDelta(songDeltasQty);
+				maxDeltas.Add(songID, maxDelta);
+			}
+			return maxDeltas;
+		}
+
+		private uint MaximizeDelta(Dictionary<uint, int> deltasQty)
+		{
+			uint delta = 0;
+			int maxOccrence = 0;
+			foreach (var pair in deltasQty)
+			{
+				if (pair.Value > maxOccrence)
+				{
+					delta = pair.Key;
+					maxOccrence = pair.Value;
+				}
+			}
+
+			return delta;
+		}
+
+		private Dictionary<uint, Dictionary<uint, List<uint>>> FilterSongs(Dictionary<uint, List<uint>> recordAddresses, Dictionary<ulong, int> quantities)
+		{
+			Dictionary<uint, Dictionary<uint, List<uint>>> res = new Dictionary<uint, Dictionary<uint, List<uint>>>();
+			Dictionary<uint, int> commonCoupleAmount = new Dictionary<uint, int>();
+			Dictionary<uint, int> commonTGZAmount = new Dictionary<uint, int>();
+			
+			foreach (var address in recordAddresses.Keys)
+			{
+				if (database.ContainsKey(address))
+				{
+					foreach (var songValue in database[address])
+					{
+						uint songID = (uint)songValue;
+						if (!commonCoupleAmount.ContainsKey(songID))
+							commonCoupleAmount.Add(songID, 0);
+						commonCoupleAmount[songID]++;
+
+						if (quantities[songValue] >= 5)							//filter out addresses that do not create TGZ
+						{
+							if (!commonTGZAmount.ContainsKey(songID))
+								commonTGZAmount.Add(songID, 0);
+							commonTGZAmount[songID]++;
+
+
+							uint AbsSongAnchTime = (uint) (songValue >> 32);
+
+							if(!res.ContainsKey(songID)) //add songID entry
+								res.Add(songID, new Dictionary<uint, List<uint>>());
+							if(!res[songID].ContainsKey(address)) //add address entry
+								res[songID].Add(address, new List<uint>());
+
+							//add the actual Absolute Anchor Time of a song
+							res[songID][address].Add(AbsSongAnchTime);
+						}
+					}
+				}
+			}
+
+#if DEBUG
+			foreach (uint songID in res.Keys)
+			{
+				int couples = commonCoupleAmount[songID];
+				int TGZs = commonTGZAmount[songID];
+				Debug.WriteLine($"Song ID: {songID} has {couples} couples where {TGZs} are in target zones: {(((double)TGZs/couples)*100):##.###} %");
+			}
+#endif
+
+
+			foreach (var songID in res.Keys)
+			{
+#if DEBUG
+				int TGZs = res[songID].Values.Count;
+				Debug.WriteLine($"SongID: {songID} has {TGZs} target zones.");
+#endif
+				//TODO: expell songs based on number of TGZs instead of portion of samples in TGZs
+				//		maybe if below 500 TGZs -> expell
+				//		WARNING: THIS DOES NOT REPRESENT TARGET ZONES! Rather it represents 
+				//maybe change to have over half of samples in TGZ
+				if (commonTGZAmount[songID] < 1200) //remove song if only few target zones are found 
+				{
+					//1200 = 300 * 5 * noiseCoefficient
+					//300 because: approx 3 samples (from parts of freq. spectrum) per ~0,1 song for 10 seconds
+					//5 because: 5 samples per targetzone
+					res.Remove(songID);
+				}
+			}
+
+			return res;
+		}
+
+		private Dictionary<ulong, int> GetSongValQuantities(Dictionary<uint, List<uint>> recordAddresses)
+		{
+			var quantities = new Dictionary<ulong, int>();
+
+			foreach (var address in recordAddresses.Keys)
+			{
+				if (database.ContainsKey(address))
+				{
+					foreach (var songValue in database[address])
+					{
+						if (!quantities.ContainsKey(songValue))
+						{
+							quantities.Add(songValue, 1);
+						}
+						else
+						{
+							quantities[songValue]++;
+
+						}
+					}
+				}
+			}
+
+			return quantities;
+		}
+
+		#region OBSOLETE
+
+		/*
 		/// <summary>
-		/// 
+		/// DELETE LATER
 		/// </summary>
-		/// <param name="recordAddresses">Key: Address, Value: AbsRecordAnchorTime</param>
-		/// <returns>[SongID, [SongAddress, &lt;AbsSongAnchorTime;Number of records&gt;]]</returns>
-		private Dictionary<uint, Dictionary<uint, int>> GetPotentialSongs(Dictionary<uint, List<uint>> recordAddresses)
+		/// <param name="recordAddresses"></param>
+		/// <returns></returns>
+		private Dictionary<uint, Dictionary<uint, int>> GetPotentialSongsObsolete(Dictionary<uint, List<uint>> recordAddresses)
 		{
 			// [SongID, [SongAddress, <AbsSongAnchorTime;Number of records>]]
 			Dictionary<uint, Dictionary<uint, int>> songTargetZones = new Dictionary<uint, Dictionary<uint, int>>();
@@ -173,13 +393,13 @@ namespace Shazam
 			Dictionary<uint, int> AddressQts = new Dictionary<uint, int>();
 			foreach (var KeyVal in recordAddresses)
 			{
-				if (databaseSmall.ContainsKey(KeyVal.Key))
+				if (database.ContainsKey(KeyVal.Key))
 				{
-					foreach (var Val in databaseSmall[KeyVal.Key])
+					foreach (var Val in database[KeyVal.Key])
 					{
-						
+
 						uint SongId = (uint)Val;
-						uint AbsAnchorTime = (uint) (Val >> 32);
+						uint AbsAnchorTime = (uint)(Val >> 32);
 
 						if (!songTargetZones.ContainsKey(SongId)) //if such song doesnt have any record yet, initialize it
 						{
@@ -197,10 +417,136 @@ namespace Shazam
 			foreach (var song in songTargetZones)
 			{
 				var a = from ocr in song.Value where ocr.Value >= 5 select ocr;
-				Debug.WriteLine($"Song: {song.Key} has {song.Value.Count} occurences with {a.Count()} target zones.");
+				Debug.WriteLine($"Song: {song.Key} has {song.Value.Count} occurences with {a.Count()} target zones: {a.Count()/(double)song.Value.Count * 100d} %");
 			}
 #endif
 
+			foreach (var songID_Dict in songTargetZones)
+			{
+				int counter = 0; //counter for number of samples in 
+				int nonTGZ = 0;
+				foreach (var AbsAnchTime_Occurece in songID_Dict.Value)
+				{
+					counter += AbsAnchTime_Occurece.Value;
+					if (AbsAnchTime_Occurece.Value < 5) //remove if occurence < 5
+					{
+						nonTGZ += AbsAnchTime_Occurece.Value;
+						songTargetZones[songID_Dict.Key].Remove(AbsAnchTime_Occurece.Key);
+						
+						continue;
+					}
+				}
+
+				int TGZ = counter - nonTGZ;
+				Debug.WriteLine($"SongID: {songID_Dict.Key} doubles: {counter} TGZs: {TGZ}");
+
+				//maybe change to have over half of samples in TGZ
+				if (counter < 250) //remove song if only few target zones are found 
+				{
+					//280 = 300 * noiseCoefficient
+					//300 because: approx 3 samples (from parts of freq. spectrum) per ~0,1 song for 10 seconds
+					songTargetZones.Remove(songID_Dict.Key);
+				}
+			}
+
+			return songTargetZones;
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="recordAddresses">[RecordAddress, (AbsRecAnchorTime)]</param>
+		/// <returns>[SongID, [SongAddress, &lt;AbsSongAnchorTime;Number of records&gt;]]</returns>
+		private Dictionary<uint, Dictionary<uint, int>> GetPotentialSongs(Dictionary<uint, List<uint>> recordAddresses)
+		{
+
+
+			Dictionary<uint, Dictionary<uint, int>> songTargetZones = new Dictionary<uint, Dictionary<uint, int>>();
+
+			#region Count occurences of values from target zones
+
+			// [SongAddress, num. of occurence]
+			Dictionary<ulong, int> AddressQts = new Dictionary<ulong, int>();
+			foreach (var RecordAddress in recordAddresses.Keys)
+			{
+				if (database.ContainsKey(RecordAddress))
+				{
+					foreach (ulong songValue in database[RecordAddress])
+					{
+						if (!AddressQts.ContainsKey(songValue)) //if such AbsAnchor has not occured yet
+						{
+							AddressQts.Add(songValue, 1);
+						}
+						else //incerement counter
+						{
+							AddressQts[songValue]++;
+						}
+					}
+				}
+			}
+
+			#endregion
+
+			// [SongID, [SongAddress, <AbsSongAnchorTime;Number of entries>]]
+			//note SongAddress must be same as RecordAddress if it is in this Dictionary
+			Dictionary<uint, Dictionary<uint, Tuple<uint, int>>> res = new Dictionary<uint, Dictionary<uint, Tuple<uint, int>>>();
+			#region Filetr out Song Addresses that only have AbsAnchors in TargetZones (Occured 5+ times)
+
+			foreach (var recordAddress in recordAddresses.Keys)
+			{
+				if (database.ContainsKey(recordAddress))
+				{
+					foreach (ulong songValue in database[recordAddress])
+					{
+						if (AddressQts[songValue] >= 5) //if the Value makes TGZ (occured more than 5 times) process it
+						{
+							uint songID = (uint) songValue;
+							uint AbsSongAnchTime = (uint) (songValue >> 32);
+
+							if (!res.ContainsKey(songID)) //create Dict instance if such entry doesnt exist yet
+							{
+								res.Add(songID, new Dictionary<uint, Tuple<uint, int>>());
+							}
+
+							if (!res[songID].ContainsKey(recordAddress)) //create first entry of such address
+							{
+								res[songID].Add(recordAddress, new Tuple<uint, int>(AbsSongAnchTime, 1));
+							}
+							else //increment entry number
+							{
+								var oldEntries = res[songID][recordAddress].Item2;
+								res[songID][recordAddress] = new Tuple<uint, int>(AbsSongAnchTime, ++oldEntries);
+							}
+						}
+					}
+				}
+			}
+
+			#endregion
+
+
+
+
+
+#if DEBUG
+			foreach (var song in res)
+			{
+				int all = 0;
+				int sum = 0;
+				foreach (var songAddress in res[song.Key])
+				{
+					foreach (var i in database[songAddress.Key])
+					{
+						all += AddressQts[i];
+					}
+					sum += songAddress.Value.Item2;
+				}
+
+				Debug.WriteLine($"SongID: {song.Key} has {all} occurences with {sum} target zones: {sum/(double)all} %");
+			}
+#endif
+			int asdsad = 1;
+			/*
 			foreach (var songID_Dict in songTargetZones)
 			{
 				int counter = 0; //counter for number of samples in 
@@ -223,10 +569,13 @@ namespace Shazam
 					songTargetZones.Remove(songID_Dict.Key);
 				}
 			}
-
+			
 			return songTargetZones;
 		}
+		*/
 
+		#endregion
+		
 		private Dictionary<uint, List<uint>> CreateRecordAddresses(List<Tuple<uint, uint>> timeFrequencyPoints)
 		{
 			Dictionary<uint, List<uint>> res = new Dictionary<uint, List<uint>>();
@@ -367,13 +716,13 @@ namespace Shazam
 
 					uint address = BuildAddress(anchorFreq, pointFreq, pointTime - anchorTime);
 
-					if (!databaseSmall.ContainsKey(address)) //create new instance if it doesnt exist
+					if (!database.ContainsKey(address)) //create new instance if it doesnt exist
 					{
-						databaseSmall.Add(address, new List<ulong>(){SongValue});
+						database.Add(address, new List<ulong>(){SongValue});
 					}
 					else //add SongValue to the list of
 					{
-						databaseSmall[address].Add(SongValue);
+						database[address].Add(SongValue);
 					}
 				}
 
@@ -383,9 +732,9 @@ namespace Shazam
 		internal uint BuildAddress(in uint anchorFreq, in uint pointFreq, uint delta)
 		{
 			uint res = anchorFreq;
-			res = res << 9; //move 9 bits 
+			res <<= 9; //move 9 bits 
 			res += pointFreq;
-			res = res << 14; //move 14 bits 
+			res <<= 14; //move 14 bits 
 			res += delta;
 			return res;
 		}
@@ -393,7 +742,7 @@ namespace Shazam
 		internal ulong BuildSongValue(in uint absAnchorTime, uint id)
 		{
 			ulong res = absAnchorTime;
-			res = res << 32;
+			res <<= 32;
 			res += id;
 			return res;
 		}
@@ -524,7 +873,6 @@ namespace Shazam
 				var normalized = 2 * Math.Sqrt((bins[i * 2] * bins[i * 2] + bins[i * 2 + 1] * bins[i * 2 + 1]) / 2048);
 				var decibel = 20 * Math.Log10(normalized);
 
-				var binMax = Tools.Tools.GetComplexAbs(bins[i * 2], bins[i * 2 + 1]);
 				if (decibel > max && decibel * coeficient > limit)
 				{
 					max = decibel;

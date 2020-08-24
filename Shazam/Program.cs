421ee830f61b0b824fcaf0eb197d85f1a112d58a
KleprlikJan <kleprlikjan@gmail.com>
2020-08-24 10:48:35 +0200

    Implement Shazam usecase

--- Shazam/Program.cs
+++ Shazam/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Data;
 using System.Diagnostics;
 using System.IO;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Threading;
+using System.Threading.Tasks;
 using SFML.Audio;
 using SFML.System;
 using Shazam.AudioProcessing;
@@ -18,9 +20,20 @@ namespace Shazam
         public static void Main()
         {
 			Shazam s = new Shazam();
-			
-			s.AddNewSong("Songs/Avicii.wav");
+			//s.AddNewSong("Songs/Avicii.wav", 1);
+			//s.AddNewSong("Songs/Coldplay.wav", 2);
+			//s.AddNewSong("Songs/400Hz.wav", 3);
+			//s.AddNewSong("Songs/Hertz.wav", 4);
+			//s.AddNewSong("Songs/Havana.wav", 5);
+			//s.AddNewSong("Songs/SameOldLove.wav", 6);	
 
-        }
+			for (uint i = 1; i <= 6; i++)
+			{
+				s.LoadSong(i);
+			}
+
+			Console.WriteLine($"Songs ID is : {s.RecognizeSong()}");
+
+		}
 	}	
 }

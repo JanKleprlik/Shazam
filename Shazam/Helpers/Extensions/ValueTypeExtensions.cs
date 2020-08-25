15161f4bb0ffd12bc2a9bec3c1d225e64e16fac0
KleprlikJan <kleprlikjan@gmail.com>
2020-08-25 20:15:34 +0200

    Add docummentation

--- Shazam/Helpers/Extensions/ValueTypeExtensions.cs
+++ Shazam/Helpers/Extensions/ValueTypeExtensions.cs
@@ -1,10 +1,18 @@
 ﻿using System;
+using System.IO;
 using System.Runtime.CompilerServices;
 
 namespace Shazam.Extensions
 {
 	public static class ValueTypeExtensions
 	{
+		/// <summary>
+		/// Swap two values
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <param name="first">Value 1</param>
+		/// <param name="second">Value 2</param>
+		/// <returns>Value 2</returns>
 		public static T Swap<T>(this T first, ref T second)
 		{
 			T tmp = second;
@@ -12,6 +20,12 @@ namespace Shazam.Extensions
 			return tmp;
 		}
 
+		/// <summary>
+		/// Returns <c>_base</c> to the power of <c>exp</c>
+		/// </summary>
+		/// <param name="_base"></param>
+		/// <param name="exp"></param>
+		/// <returns></returns>
 		public static int ToPowOf(this int _base, int exp) 
 		{
 			int res = _base;
@@ -23,10 +37,14 @@ namespace Shazam.Extensions
 			return res;
 		}
 
+		/// <summary>
+		/// Returns second power of <c>_base</c>
+		/// </summary>
+		/// <param name="_base"></param>
+		/// <returns></returns>
 		public static double Pow2(this double _base)
 		{
 			return _base * _base;
 		}
-
 	}
 }

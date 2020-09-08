9b1e9ae1843e40c7ee0978a94d74c3cd33cc32a7
KleprlikJan <kleprlikjan@gmail.com>
2020-09-08 18:45:54 +0200

    Add contstant documentation

--- Shazam/Shazam/Constants.cs
+++ Shazam/Shazam/Constants.cs
@@ -8,14 +8,37 @@ namespace Shazam
 	{
 		static class Constants
 		{
+			/// <summary>
+			/// Default size of FFT window
+			/// </summary>
 			public const int WindowSize = 4096;
+			/// <summary>
+			/// Default downsample coeficient
+			/// </summary>
 			public const int DownSampleCoef = 4;
+			/// <summary>
+			/// Default size of target zone
+			/// </summary>
 			public const int TargetZoneSize = 5;
+			/// <summary>
+			/// Default offset of anchor from first actual point
+			/// </summary>
 			public const int AnchorOffset = 2;
+			/// <summary>
+			/// Obligated portion of samples in TGZ
+			/// </summary>
 			public const double SamplesInTgzCoef = 0.5;
+			/// <summary>
+			/// Obligated portion of time coherent notes
+			/// </summary>
 			public const double CoherentNotesCoef = 0.4;
-
+			/// <summary>
+			/// Default path to fingerprints
+			/// </summary>
 			public const string FingerprintPath = @"Resources/Fingerprints";
+			/// <summary>
+			/// Default path to metadata
+			/// </summary>
 			public const string MetadataPath = @"Resources/Metadata.csv";
 
 		}
